f1f24a30155616e1568a18ea4a52483bd353a068
sunxu <sunxu.me@etutech.com>
2020-09-24 16:25:03 +0800

    [Update] add py

---
+++ CheckContentView-OC.py
@@ -0,0 +1,57 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8') #解决python2.x的str默认是ascii编码和unicode编码冲突冲突问题
+from Tkinter import *
+import tkFileDialog
+import tkMessageBox
+import os
+import re
+import json
+import random
+import fileinput
+
+# 检查 UITableViewCell、UICollectionViewCell 子视图是否添加到 contentview 上，如果没有添加，输出当前类名以及所在行
+def matchATSMethod(dir_path, rematch):
+    suf_set = ('.h') # 只需要遍历 .h 文件，就可以确定，是否是 UITableViewCell、UICollectionViewCell 类
+    interface_symbol_containing = ': ' + rematch
+    print('\n 继承自 ' + rematch + ' 的类 ' + '\n')
+    for (root, dirs, files) in os.walk(dir_path):
+        
+        for file_name in files:
+            if file_name.endswith(suf_set):
+                with open(os.path.join(root, file_name), 'r+') as f:
+                    line = f.readline()
+                    while line:
+                        if interface_symbol_containing in line: # 命中
+                            checkATSMethod(root, file_name, rematch)
+                            break
+                        line = f.readline()
+                    f.close()
+
+
+# 将 .h 文件路径转为 .m 文件路径
+def getOCFileName(s):
+    return s[::-1].replace("h", "m", 1)[::-1]  
+
+# 打开 .m 文件，遍历每一行，是否符合 [self addSubviews:] 格式，符合输出当前类名、以及所在行
+def checkATSMethod(root, file_name, match):
+    ats_symbol_containing = '[self addSubview:'
+    file_name = getOCFileName(file_name)
+    with open(os.path.join(root, file_name), 'r+') as f:
+        line = f.readline()    
+        idx = 0 
+        while line:
+            idx += 1
+            if ats_symbol_containing in line: # 命中 
+                print(file_name + ' 第 ' + str(idx) + ' 行, 路径为' + root + '\n' )  
+                idx = 0
+                break
+            line = f.readline()
+        f.close()
+
+# 直接指定源码根路径就可以, 以及想要排查的类
+matchATSMethod('/Users/xx/xx/xx/xx/Pods', 'UICollectionViewCell') 
+matchATSMethod('/Users/xx/xx/xx/xx/Pods', 'UITableViewCell')            
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CheckContentView
+
+用于检查 iOS 项目中，自定义的 UITableviewCell、UICollectionViewCell 中子视图没有添加到 ContentView 的脚本，支持 OC、Swift
+
+#### 实现思路
+
+##### 首先确定哪些类是  UITableviewCell、UICollectionViewCell 
+
+在 OC 语言的项目中，同时存在 h&m 文件，确定是什么类的时候，只需要打开 h文件就可以，同时校验是否继承自   UITableviewCell、UICollectionViewCell 
+
+即符合 ：UITableviewCell、UICollectionViewCell  这样的语法结构
+
+##### 确定没有添加到 ContentView 上
+
+正常添加到 ContentView 上的语法规则是 self.contentView addSubview，如果调用的是 self addSubview 就是没有添加到 ContentView 上，由此确定
+
+
+
